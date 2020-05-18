b2809f0a4529fcf2632a876f5ac2378c2afdc639
Rajat R Hande <rajatrhande@Rajats-MacBook-Pro.local>
2020-05-17 23:54:42 -0400

    trying to update to d4

--- CricVis/cricVis/src/app/charts/bar-chart/bar-chart.component.ts
+++ CricVis/cricVis/src/app/charts/bar-chart/bar-chart.component.ts
@@ -10,13 +10,91 @@ declare var d3: any;
 })
 
 export class BarChartComponent implements OnInit {
-  @ViewChild('myDiv1', { static: true }) myDiv: ElementRef;
-  
+  @ViewChild('barChartDiv', { static: true }) myDiv: ElementRef;
 
+  barChartData = {
+    width: 300,
+    height: 220,
+    margin: {
+      left: 50,
+      right: 10,
+      bottom: 10,
+      top: 20
+    },
+    data: [
+      { 'year': 2016, 'count': 45 },
+      { 'year': 2015, 'count': 22 },
+      { 'year': 2014, 'count': 99 }
+    ]
+  }
+  svg: any;
 
   constructor(private dataService: CommonDataService) { }
   ngOnInit() {
     this.dataService.refreshChartsSubject.subscribe(s => {
     })
+
+    this.barChart()
+  }
+
+  barChart() {
+
+    var x = d3.scale.ordinal().range([0, this.barChartData.width]);
+
+    var y = d3.scale.linear().range([this.barChartData.height, 0]);
+
+
+
+    var svg =
+      d3.select(this.myDiv.nativeElement)
+        .append("svg")
+        .attr("width", this.barChartData.width + this.barChartData.margin.left + this.barChartData.margin.right)
+        .attr("height", this.barChartData.height + this.barChartData.margin.top + this.barChartData.margin.bottom)
+        .append("g")
+        .attr("transform",
+          "translate(" + this.barChartData.margin.left + "," + this.barChartData.margin.top + ")");
+
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+    
+    x.domain(this.barChartData.data.map(function (d) { console.log(d['year']); return d['year']; }));
+    y.domain([0, d3.max(this.barChartData.data, function (d) { return d['count']; })]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + this.barChartData.height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Value ($)");
+
+    svg.selectAll("bar")
+      .data(this.barChartData.data)
+      .enter()
+      .append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function (d) { console.log(x(d.year)); return x(d.year); })
+      .attr("width", x.bandwidth())
+      .attr("y", function (d) { return y(d.count); })
+      .attr("height", d => { return this.barChartData.height - y(d.count); });
   }
 }
+
--- CricVis/cricVis/src/app/charts/parallel-coord/parallel-coord.component.ts
+++ CricVis/cricVis/src/app/charts/parallel-coord/parallel-coord.component.ts
@@ -11,11 +11,10 @@ declare var d3: any;
 export class ParallelCoordComponent implements OnInit {
   @ViewChild('myDiv', { static: true }) myDiv: ElementRef;
 
-  color = d3.scale.category10()
-  // function(d) { return d3.scale.linear()
-  //   .domain([0, 150])
-  //   .range(["red", "green"])
-  //   .interpolate(d3.interpolateLab)(d[0]) };
+  color = d3.scale.ordinal()
+    .domain(["W", "L"])
+    .range(["red", "green"])
+  //d3.scale.category10()
 
   constructor(public dataService: CommonDataService) {
   }
@@ -25,24 +24,30 @@ export class ParallelCoordComponent implements OnInit {
       this.myDiv.nativeElement.innerHTML = '';
       let data = []
       this.dataService.filteredPlayerScoreCard.forEach(inning => {
-        
+
         let row = []
-        data.push({'Runs': this.nullCheck(inning.r),
+        data.push({
+          'Runs': this.nullCheck(inning.r),
           'Balls Taken': inning.b,
           'Strike Rate': inning.sr,
           'Fours': inning.fours,
           'Sixes': inning.sixes,
-          'Dismissal Mode': this.nullCheck(inning.dismissedMethod, 'dm')
+          'Dismissal Mode': this.nullCheck(inning.dismissedMethod, 'dm'),
+          'Result': inning.result
         })
       })
 
-      d3.parcoords({dimensionTitles: {}})(this.myDiv.nativeElement)
-      .alpha(0.7).color(this.color)
-      .data(data).render().createAxes().brushMode("1D-axes");
+      d3.parcoords({ dimensionTitles: {} })(this.myDiv.nativeElement)
+        .alpha(0.7).
+        data(data).
+        color((d) => {
+          this.color(d.Result)
+        }).
+        render().createAxes().brushMode("1D-axes");
     })
   }
 
-  nullCheck(val, col='none') {
+  nullCheck(val, col = 'none') {
     if (val == null) {
       if (col == 'dm') {
         return 'Not Out'
